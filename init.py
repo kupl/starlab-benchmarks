b99f3afda4688ac2b8ee41f5dc7e9a213f63711c
seongjoonh <seongjoon@korea.ac.kr>
2023-10-27 15:12:08 +0900

    fix tar link

--- init.py
+++ init.py
@@ -1,4 +1,12 @@
 from safety import C
+import argparse
 
 if __name__ == "__main__":
-    C.initialize()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--safety-c", action="store_true", help="initialize safety C bugs"
+    )
+
+    args = parser.parse_args()
+    if args.safety_C:
+        C.initialize()
--- safety/C/config.py
+++ safety/C/config.py
@@ -2,5 +2,5 @@ import os
 from pathlib import Path
 
 BENCHMARK_ROOT = Path((os.path.abspath(__file__))).parent
-INFER_OUT_TAR_URL = "https://github.com/kupl/starlab-benchmarks/releases/download/Safety-C/infer-out.tar.gz"
+INFER_OUT_TAR_URL = "https://github.com/kupl/starlab-benchmarks/releases/download/Safety-C/projects.tar.gz"
 PROJECTS_DIR = BENCHMARK_ROOT / "projects"
