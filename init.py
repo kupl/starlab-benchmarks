b0e561895aed2be6da77f2adc9891bfe28b210f0
seongjoonh <seongjoon@korea.ac.kr>
2023-11-02 18:56:30 +0900

    [scripts] fix typo in main initializer

--- init.py
+++ init.py
@@ -3,10 +3,7 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--safety-c", action="store_true", help="initialize safety C bugs"
-    )
-
+    parser.add_argument("--safety-c")
     args = parser.parse_args()
-    if args.safety_C:
+    if args.safety_c:
         C.initialize()
