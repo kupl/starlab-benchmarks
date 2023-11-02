ea155985251f31d650fbf6a0c249a8e8482d132e
seongjoonh <seongjoon@korea.ac.kr>
2023-11-02 10:26:42 +0900

    [scripts] rename infer-out tar to `projects.tar.gz`

--- safety/C/initialize.py
+++ safety/C/initialize.py
@@ -4,7 +4,7 @@ from .config import INFER_OUT_TAR_URL, PROJECTS_DIR
 
 
 def initialize():
-    tar_name = "infer-out.tar.gz"
+    tar_name = "projects.tar.gz"
     wget_command = f"wget {INFER_OUT_TAR_URL} -O {tar_name}"
     tar_command = f"tar xvzf {tar_name} -C {PROJECTS_DIR} --strip-components=1"
 
