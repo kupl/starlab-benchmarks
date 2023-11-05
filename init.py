1b81bd3298d2587d0a87a02c3717be829c82979e
seongjoonh <seongjoon@korea.ac.kr>
2023-11-05 17:00:52 +0900

    [scripts] make --safety-c to optional

--- init.py
+++ init.py
@@ -3,7 +3,7 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--safety-c")
+    parser.add_argument("--safety-c", action='store_true')
     args = parser.parse_args()
     if args.safety_c:
         C.initialize()
